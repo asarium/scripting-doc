2d3808dc3ba23fcaa8b41b060b9a212b0b8bd9e8
asarium <asarium@gmail.com>
2021-01-06 17:16:35 +0100

    Add action to export Lua definitions

--- src/components/doc/DocumentationView.tsx
+++ src/components/doc/DocumentationView.tsx
@@ -1,15 +1,27 @@
-import {Typography} from "@material-ui/core";
-import {observer} from "mobx-react-lite";
+import { Fab, makeStyles, Tooltip, Typography } from "@material-ui/core";
+import { observer } from "mobx-react-lite";
 import * as React from "react";
-import {useDocStore} from "../../state/useStores";
+import { download } from "../../util/download";
+import { useDocStore } from "../../state/useStores";
+import LuaIcon from "../icons/LuaIcon";
 import LoadingIndicator from "../LoadingIndicator";
 import DocElementList from "./DocElementList";
 import EnumsView from "./EnumsView";
 import FilteredDocElementList from "./FilteredDocElementList";
 import StringListView from "./StringListView";
 
+const useStyles = makeStyles(theme => ({
+    luaFab: {
+        position: 'fixed',
+        bottom: theme.spacing(4),
+        right: theme.spacing(6),
+    },
+}));
+
 const DocumentationView: React.FC = observer(() => {
     const documentationStore = useDocStore();
+
+    const classes = useStyles();
 
     if (documentationStore.errorText) {
         return (<Typography>
@@ -18,7 +30,7 @@ const DocumentationView: React.FC = observer(() => {
     }
 
     if (documentationStore.loadingDocumentation) {
-        return <LoadingIndicator/>;
+        return <LoadingIndicator />;
     }
 
     const doc = documentationStore.documentation!;
@@ -28,20 +40,31 @@ const DocumentationView: React.FC = observer(() => {
     if (filteredElements === null) {
         elementList = (
             <>
-                <DocElementList elements={doc.elements.filter(x => x.type === "library")}/>
-                <DocElementList elements={doc.elements.filter(x => x.type === "class")}/>
+                <DocElementList elements={doc.elements.filter(x => x.type === "library")} />
+                <DocElementList elements={doc.elements.filter(x => x.type === "class")} />
             </>
         );
     } else {
-        elementList = <FilteredDocElementList elements={filteredElements}/>;
+        elementList = <FilteredDocElementList elements={filteredElements} />;
     }
+
+    const generateLuaDefs = () => {
+        const luaDoc = doc.generateLuaDoc();
+
+        download("fso.lua", luaDoc);
+    };
 
     return (
         <>
             {elementList}
-            <StringListView title={"Actions"} strings={doc.actions.map(x => x.name)}/>
-            <StringListView title={"Conditions"} strings={doc.conditions}/>
-            <EnumsView enums={doc.enums}/>
+            <StringListView title={"Actions"} strings={doc.actions.map(x => x.name)} />
+            <StringListView title={"Conditions"} strings={doc.conditions} />
+            <EnumsView enums={doc.enums} />
+            <Tooltip title="Generate Lua definition">
+                <Fab className={classes.luaFab} color="primary" onClick={generateLuaDefs} >
+                    <LuaIcon />
+                </Fab>
+            </Tooltip>
         </>
     );
 });
